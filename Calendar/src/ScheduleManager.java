15869ec12a97fb555334403acfa051b20a84619f
OVUSA <velichko.olga.chicago@gmail.com>
2021-04-13 21:21:59 -0500

    created a menu for basic navigation.

---
+++ Calendar/src/ScheduleManager.java
@@ -0,0 +1,13 @@
+
+public class ScheduleManager{
+	AccountManager accountManager;
+	
+	public void createEvenr() {
+		System.out.println("Do you want to schedule an event?\n"
+				+ "Enter the name of the e");
+		
+		
+	}
+	
+	
+}
